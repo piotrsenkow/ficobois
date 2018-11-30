53511eb2fbc3b94e8aff3e0e4bf41f0d921e1aaa
Piotr Senkow <piotr.senkow@lanzatech.com>
2018-11-30 16:13:43 -0600

    changing of class

--- ficobois/ficobois.py
+++ ficobois/ficobois.py
@@ -3,19 +3,19 @@ import math
 """Main module."""
 
 
-class A:
-    def __init__(self, x):
-        self.x = x
-
-    def sqrt(self):
-        answer = math.sqrt(self.x)
+class MathTool:
+    @staticmethod
+    def sqrt(x):
+        answer = math.sqrt(x)
         return answer
 
-    def square(self):
-        answer = self.x**2
+    @staticmethod
+    def square(x):
+        answer = x**2
         return answer
 
-    def square_then_sqrt(self):
-        answer = self.x**2
+    @staticmethod
+    def square_then_sqrt(x):
+        answer = x**2
         square_root = math.sqrt(answer)
         return square_root
