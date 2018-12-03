d52dab414afeaac2e4e1b98ed703dc2d742c50cb
Piotr Senkow <piotr.senkow@lanzatech.com>
2018-12-03 10:56:14 -0600

    change to package

--- ficobois/ficobois.py
+++ ficobois/ficobois.py
@@ -3,19 +3,17 @@ import math
 """Main module."""
 
 
-class MathTool:
-    @staticmethod
-    def sqrt(x):
-        answer = math.sqrt(x)
-        return answer
+def sqrt(x):
+    answer = math.sqrt(x)
+    return answer
 
-    @staticmethod
-    def square(x):
-        answer = x**2
-        return answer
 
-    @staticmethod
-    def square_then_sqrt(x):
-        answer = x**2
-        square_root = math.sqrt(answer)
-        return square_root
+def square(x):
+    answer = x**2
+    return answer
+
+
+def square_then_sqrt(x):
+    answer = x**2
+    square_root = math.sqrt(answer)
+    return square_root
