16fe13a59bed053d1567f997aad27223ffa9c95e
Piotr Senkow <piotr.senkow@lanzatech.com>
2018-12-03 11:57:49 -0600

    fixed unit tests

--- ficobois/__init__.py
+++ ficobois/__init__.py
@@ -4,7 +4,4 @@
 
 __author__ = """Piotr Senkow"""
 __email__ = 'piotrsenkow@gmail.com'
-__version__ = '0.1.0'
-
-from ficobois.ficobois import A
-
+__version__ = '0.1.4'
--- tests/test_ficobois.py
+++ tests/test_ficobois.py
@@ -5,10 +5,8 @@
 
 
 import unittest
-from click.testing import CliRunner
 
-from ficobois import ficobois
-from ficobois import cli
+from ficobois import ficobois as fb
 
 
 class TestFicobois(unittest.TestCase):
@@ -22,27 +20,17 @@ class TestFicobois(unittest.TestCase):
 
     def test_sqrt_function(self):
         """Test something."""
-        answer = ficobois.sqrt(25)
+        answer = fb.sqrt(25)
         shouldbe = 5.0
         self.assertEqual(answer, shouldbe)
 
     def test_square_function(self):
-        answer = ficobois.square(4)
+        answer = fb.square(4)
         self.assertEqual(answer, 16)
 
     def test_square_then_sqrt(self):
-        answer = ficobois.square_then_sqrt(3)
+        answer = fb.square_then_sqrt(3)
         self.assertEqual(answer, 3.0)
-
-    def test_command_line_interface(self):
-        """Test the CLI."""
-        runner = CliRunner()
-        result = runner.invoke(cli.main)
-        assert result.exit_code == 0
-        assert 'ficobois.cli.main' in result.output
-        help_result = runner.invoke(cli.main, ['--help'])
-        assert help_result.exit_code == 0
-        assert '--help  Show this message and exit.' in help_result.output
 
 
 if __name__ == '__main__':
